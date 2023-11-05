73d07e4e35a2ee771f2db1aae9d766c75f96fd10
Peter Bakshaev <bak.pet@gmail.com>
2023-11-05 19:43:55 +0200

    chapter 4

--- course-project/dist/app.js
+++ course-project/dist/app.js
@@ -1,4 +1,9 @@
 "use strict";
 console.log('Helld world TS!!!!!!@@@@@@@@');
-function add(n1, n2) {
+function add(n1, _n2) {
+    let x = 1;
+    if (n1 > 10)
+        return n1;
+    return x;
+    return;
 }
--- course-project/src/app.ts
+++ course-project/src/app.ts
@@ -1,8 +1,8 @@
 console.log('Helld world TS!!!!!!@@@@@@@@');
 
-function add(n1: number, n2: number) {
-  //   let x = 1;
-  //   if (n1 > 10) return n1;
-  //   return x;
-  //   return;
+function add(n1: number, _n2: number) {
+  let x = 1;
+  if (n1 > 10) return n1;
+  return x;
+  return;
 }
